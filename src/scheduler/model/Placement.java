27a93bce14903f759f4d0a1752465c0790d959fd
arjinozceylan <arjin.ozceylan@std.ieu.edu.tr>
2025-11-29 17:22:47 +0300

    şuanda bir öğrencinin aynı günde 3 sınavı oluyordu  o çözüldü ama 3000 öğrenci ile bile çalışıyor

--- src/scheduler/model/Placement.java
+++ src/scheduler/model/Placement.java
@@ -11,7 +11,7 @@ public class Placement {
         this.courseId = courseId;
         this.timeslot = timeslot;
         this.classrooms = classrooms;
-    }//aaaaa
+    }//aaaaaaaa
 
     @Override
     public boolean equals(Object obj) {
