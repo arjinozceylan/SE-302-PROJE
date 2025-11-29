ac87f99f298985765b979b09bc54aae440099780
SilaKarabag <silakrb2@gmail.com>
2025-11-29 18:32:13 +0300

    Fixed JavaFX runtime components missing error by adding Launcher class

---
+++ src/scheduler/ui/Launcher.java
@@ -0,0 +1,11 @@
+package scheduler.ui;
+
+public class Launcher {
+        public static void main(String[] args) {
+            // Bu sınıf Application'dan türemediği için Java hata vermez.
+            // Buradan asıl uygulamayı çağırdığımızda kütüphaneler doğru yüklenir.
+            MainApp.main(args);
+        }
+    }
+
+
